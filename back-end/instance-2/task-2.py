73d4996fefe987170fe570c4b561cc3e0ffa39b1
rovshan <rouvsen@gmail.com>
2023-10-31 05:39:07 +0400

    Update task-2 related instance-2

--- back-end/instance-2/task-2.py
+++ back-end/instance-2/task-2.py
@@ -6,5 +6,7 @@ myDict = {
 enteredNum = int(input("Enter a number, 1: Keys, 2: Values. Your choice is: "))
 if enteredNum == 1:
     print('keys: ' + str(list(myDict.keys())))  # way1
-else:
+elif enteredNum == 2:
     print('values: ' + ', '.join(myDict.values()))  # way2
+else:
+    print('You entered wrong num, please try again..')
