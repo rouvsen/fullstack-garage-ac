e25e0f01a316816c3f2d6b87516f49d5520f8f73
rovshan <rouvsen@gmail.com>
2023-11-02 00:43:07 +0400

    Add task-2 to the instance-3 on the back-end layer

---
+++ back-end/instance-3/task-2.py
@@ -0,0 +1,8 @@
+num = 1
+
+while num <= 10:
+    willBeIncreased = 1
+    while willBeIncreased <= 10:
+        print(f'{num} * {willBeIncreased} = ' + str((num * willBeIncreased)))
+        willBeIncreased += 1
+    num += 1
