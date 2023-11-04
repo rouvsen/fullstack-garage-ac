0d89bc082de423372b72d0fc248d1ae4a3518d55
rovshan <rouvsen@gmail.com>
2023-11-04 14:31:12 +0400

    Update task-1 in the instance-4 back-end layer

--- back-end/instance-4/task-1.py
+++ back-end/instance-4/task-1.py
@@ -1,1 +1,16 @@
-# task-1
+import os
+
+clientAnswer = int(input("(1) TXT Faylini sil (2) Melumat Elave et: "))
+
+if clientAnswer == 1:
+    if os.path.exists("text.txt"):
+        os.remove("text.txt")
+        print("text.txt file deleted.")
+    else:
+        print("text.txt file yaradin, sonra silmek ucun cehd edin..")
+elif clientAnswer == 2:
+    with open("text.txt", "a") as f:
+        f.write("Value\n")
+        print("Deyer elave edildi")
+else:
+    print("Daxil edilen secim 1 ve ya 2 deyil !!")
