8824f677a0864c416cefbd9c73c58bb7c4b6e8f3
rovshan <rouvsen@gmail.com>
2023-11-04 14:47:54 +0400

    Update task-2 in the instance-4 back-end layer

--- back-end/instance-4/task-2.py
+++ back-end/instance-4/task-2.py
@@ -1,1 +1,9 @@
-# task-2
+# add mock values to file
+with open("text.txt", "a") as f:
+    f.write("Python\n")
+    f.write("Java\n")
+    f.write("JavaScript\n")
+# get first and last letters of value
+with open("text.txt", "r") as r:
+    for el in r.readlines():
+        print("first letter: " + el[0], "last letter: " + el[len(el) - 2])
